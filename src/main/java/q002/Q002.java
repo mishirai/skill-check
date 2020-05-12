185e639437e5cbf95d0ccc2b72305422820dd436
Minako <minakoshiraiitbiz@gmail.com>
2020-05-12 17:32:16 +0900

    WIP:完了

--- src/main/java/q002/Q002.java
+++ src/main/java/q002/Q002.java
@@ -1,5 +1,8 @@
 package q002;
-
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Comparator;
 /**
  * Q002 並べ替える
  *
@@ -45,5 +48,41 @@ public class Q002 {
             "14,林",
             "9,清水"
     };
+    public static void main(String[] args) {
+        Arrays.sort(dataList);
+        System.out.println(Arrays.asList(dataList));
+
+        ArrayList<Person> persons = new ArrayList<>();
+        for (String line : dataList) {
+            String[] strData= line.split(",",0);
+            persons.add(new Person(Integer.parseInt(strData[0]),strData[1]));
+        }
+        Collections.sort(persons, new Comparator<Person>() {
+            public int compare(Person personFirst, Person personSecond) {
+                return Integer.compare(personFirst.getId(), personSecond.getId());
+            }
+        });
+        for (int i = 0; i < dataList.length; i++){
+            System.out.println(persons.get(i));
+        }
+    }
+    public static class Person  {
+        private Integer id;
+        private String name;
+
+        public Person(Integer id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+        public int getId() {
+            return this.id;
+        }
+        public String getName() {
+            return this.name;
+        }
+        public String toString() {
+            return id+ "," + name;
+        }
+    }
 }
-// 完成までの時間: xx時間 xx分
+// 完成までの時間: 4時間 0分
--- src/main/java/q003/Q003.java
+++ src/main/java/q003/Q003.java
@@ -1,6 +1,13 @@
 package q003;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.InputStreamReader;
+import java.io.FileNotFoundException;
+import java.util.TreeMap;
 
 /**
  * Q003 集計と並べ替え
@@ -33,5 +40,47 @@ public class Q003 {
     private static InputStream openDataFile() {
         return Q003.class.getResourceAsStream("data.txt");
     }
+    public static void main(String[] args) {
+        String line = null;
+        String word = null;
+        String[] exWord = {",","."};
+        int i = 0;
+        int cnt = 0;
+        int wordSize = 0;
+        Map<String, Integer> m = new HashMap<>();
+        try {
+            InputStream is = openDataFile();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            while ((line = br.readLine()) != null) {            //n行読み込み
+                String[] strWord= line.split(" ",0);
+                for (i=0; i < strWord.length;i++){
+                    word = strWord[i];
+                    wordSize = word.length();
+                    //","と"."を取り除く
+                    if ((word.substring(wordSize - 1)).equals(exWord[0])
+                            || (word.substring(wordSize - 1)).equals(exWord[1])){
+                        word = word.substring(0,wordSize-1);
+                    }
+                    //文字数のカウント
+                    if (m.containsKey(word)){
+                        cnt = m.get(word) +1;
+                        m.replace(word,cnt);
+                    }else{
+                        m.put(word,1);
+                    }
+                }
+            }
+            TreeMap<String, Integer> treeMap = new TreeMap<String, Integer>(m);
+            System.out.println("TreeMap");
+            for (String key : treeMap.keySet()) {
+                System.out.println(key + " = " + treeMap.get(key));
+            }
+        } catch (FileNotFoundException e1) {
+            System.out.println("File is not exist.");
+        } catch (IOException e2) {
+            e2.printStackTrace();
+        }
+    }
+
 }
-// 完成までの時間: xx時間 xx分
+// 完成までの時間: 8時間 xx分
